663e0a5227e961a896fb86d24f1ebe05069c26ad
Michael Bashford <mikebashford@gmail.com>
2022-01-27 18:04:59 -0600

    Added hashing to passwords and validating user login

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -50,15 +50,39 @@ router.post('/', (req, res) => {
     });
 });
 
+router.post('/login', (req, res) => {
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+    User.findOne({
+      where: {
+        email: req.body.email
+      }
+    }).then(dbUserData => {
+      if (!dbUserData) {
+        res.status(400).json({ message: 'No user with that email address!' });
+        return;
+      }
+  
+      const validPassword = dbUserData.checkPassword(req.body.password);
+      if (!validPassword) {
+        res.status(400).json({ message: 'Incorrect password!' });
+        return;
+      }
+      
+      res.json({ user: dbUserData, message: 'You are now logged in!' });
+  
+    });  
+  });
+
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
 
   // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
   User.update(req.body, {
-    where: {
-      id: req.params.id
-    }
+      individualHooks: true,
+      where: {
+        id: req.params.id
+      }
   })
     .then(dbUserData => {
       if (!dbUserData[0]) {
@@ -72,6 +96,8 @@ router.put('/:id', (req, res) => {
       res.status(500).json(err);
     });
 });
+
+
 
 // DELETE /api/users/1
 router.delete('/:id', (req, res) => {
