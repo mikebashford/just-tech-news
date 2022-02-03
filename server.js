f5c80740ef6df3473d7e9336dc0eeea7ab298af4
Michael Bashford <mikebashford@gmail.com>
2022-02-03 15:07:54 -0600

    Added improvements to post information and tests

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const sequelize = require("./config/connection");
 const path = require("path");
 const exphbs = require("express-handlebars");
 const session = require("express-session");
-const hbs = exphbs.create({});
+const helpers = require("./utils/helpers");
+const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
